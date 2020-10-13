f6568ed1df5b02246c902178aaacb20431baf1eb
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-10-13 19:58:07 -0300

    upload stylesheet

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
-const daysElements = document.getElementById('days');
-const hoursElements = document.getElementById('hours');
-const minutesElements = document.getElementById('minutes');
-const secondsElements = document.getElementById('secondes');
+// select elements
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
 
-// Release date of
-const launch = "07 Oct 2021";
+// final date
+const launch = "31 Dec 2020";
 
-// countdown function
+// countdown
 function countdown() {
-    // taking time
     const launchDate = new Date(launch);
     const currentDate = new Date();
 
     const totalSeconds = (launchDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes  = Math.floor(totalSeconds / 60) % 60;
+    const hours = Math.floor(totalSeconds / 3600 % 24);
+    const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    // entering the time on the screen
-    daysElements.innerHTML = days;
-    hoursElements.innerHTML = formatTime(hours);
-    minutesElements.innerHTML = formatTime(minutes);
-    secondsElements.innerTHML = formatTime(seconds);
-}
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = hours;
+    minutesElement.innerHTML = minutes;
+    secondsElement.innerHTML = seconds;
 
-function formatTime(time) {
-    return time < 10 ? (`0${time}`) : time;
 }
 
 // start countdown
 countdown();
 
+// interval for countdown
 setInterval(countdown, 1000);
